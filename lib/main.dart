8a0b2795656a93428434f62ecdd03e01d8606f7a
ndwMango <146925387+ndwMango@users.noreply.github.com>
2024-06-18 01:34:07 +0900

    다시 공부중

--- lib/main.dart
+++ lib/main.dart
@@ -1,120 +1,39 @@
 import 'package:flutter/material.dart';
 
-// dart컴파일러가 컴파일하면 두 변수를 위한 메모리 공간을 만드는 대신
-const taxAmount = 15;
-const priceAmount = 15;
-
-var finalPrice = 46;
-
 class Player {
-  String name;
-  Player({required this.name});
+  String? name;
+  Player();
 }
 
 void main() {
   var nico = Player(name: 'potato');
+
   runApp(const App());
 }
 
-// MaterialApp 은 안드로이드 스타일, CupertinoApp 은 ios 스타일, 구글이 만들었으므로 MaterialApp가 더 자연스러움
 class App extends StatelessWidget {
   const App({super.key});
 
-  //StatelessWidget 는 그림만 그려주는 위젯
   @override
   Widget build(BuildContext context) {
-    // Scaffold가 없으면 0,0부터 그림을 그림
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 40,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 90,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment:
-                        CrossAxisAlignment.end, //컬럼은 가로방향, 로우는 세로방향
-                    children: [
-                      Text(
-                        'Hey, Sellina',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      Text(
-                        'Welcome back',
-                        style: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 0.8),
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 120,
-              ),
-              Text(
-                'Total Balance',
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.white.withOpacity(0.8),
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Text('Hey, Selena'),
+                    Text('Welcome back'),
+                  ],
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                '\$5 194 482',
-                style: TextStyle(
-                  fontSize: 44,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(45),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 15,
-                        horizontal: 50,
-                      ),
-                      child: Text(
-                        'Transfer',
-                        style: TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
-// git Test
-// git Test2
